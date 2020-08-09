00d304db543e0430b8ac60febfb59d180b94d1f1
wenpin <wpchou@outlook.com>
2020-08-09 14:18:51 +0800

    2. Add Two Numbers

---
+++ src/main/java/leetcode/LC2.java
@@ -0,0 +1,71 @@
+package leetcode;
+
+public class LC2 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(final int val) { this.val = val; }
+        ListNode(final int val, final ListNode next) { this.val = val; this.next = next; }
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final ListNode other = (ListNode) obj;
+			if (val != other.val)
+				return false;
+                        if (next == null && other.next == null) return true;
+			return next.equals(other.next);
+		}
+		@Override
+		public String toString() {
+                    return String.valueOf(val) + (next == null ? "." : "-" + next.toString());
+		}
+    }
+
+    public ListNode solution1(final ListNode l1, final ListNode l2) {
+        // final var sum = l1.val + l2.val;
+        // final var hasCarry = sum >= 10;
+        // return new ListNode(
+        //                 (hasCarry?sum:sum-10),
+        //                 solution1Helper(l1.next, l2.next, (hasCarry?1:0)));
+        return solution1Helper(l1, l2, 0);
+    }
+
+    private ListNode solution1Helper(final ListNode l1, final ListNode l2, final int carry) {
+        System.out.println("-------------------------------------------");
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(carry);
+        System.out.println("-------------------------------------------");
+        if (l1 != null && l2 != null) {
+            final var sum = l1.val + l2.val + carry;
+            if (sum < 10) {
+                return new ListNode(sum, solution1Helper(l1.next, l2.next, 0));
+            } else {
+                return new ListNode(sum - 10, solution1Helper(l1.next, l2.next, 1));
+            }
+        } else if (l1 != null && l2 == null) {
+            final var sum = l1.val + carry;
+            if (sum < 10) {
+                return new ListNode(sum, l1.next);
+            } else {
+                return new ListNode(sum - 10, solution1Helper(l1.next, null, 1));
+            }
+        } else if (l1 == null && l2 != null){
+            return solution1Helper(l2, l1, carry);
+        } else {
+            // l1 == null && l2 == null
+            if (carry == 0) {
+                return null;
+            } else {
+                return new ListNode(1);
+            }
+        }
+    }
+}
---
+++ src/test/java/leetcode/LC2Test.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+public class LC2Test {
+    @Test public void soluction1Test() {
+        var lc = new LC2();
+        // assertEquals(buildList(new int[]{0}), buildList(new int[]{0}));
+        // assertEquals(buildList(new int[]{0}),
+                     // lc.solution1(buildList(new int[]{0}), buildList(new int[]{0})));
+        // assertEquals(buildList(new int[]{9,9,9}),
+        //              lc.solution1(buildList(new int[]{0}), buildList(new int[]{9,9,9})));
+        assertEquals(buildList(new int[]{0, 0, 0, 1}),
+                     lc.solution1(buildList(new int[]{1}), buildList(new int[]{9,9,9})));
+    }
+
+    private LC2.ListNode buildList(int[] list) {
+        // TODO use stream reduce
+        // return Arrays.stream(list)
+        //     .reduce((LC2.ListNode)null, (LC2.ListNode acc, Int item) -> {
+        //         return new LC2.ListNode(item, acc);
+        //     });
+        var header = new LC2.ListNode();
+        var cursor = header;
+        for (var item : list) {
+            cursor.next = new LC2.ListNode(item);
+            cursor = cursor.next;
+        }
+        System.out.println(header.next);
+        return header.next;
+    }
+}
