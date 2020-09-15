3d942f44891fc570ce58681fda007d78634f8e4a
wenpin <wpchou@outlook.com>
2020-09-15 11:51:27 +0800

    fix

--- src/main/java/leetcode/LC14.java
+++ src/main/java/leetcode/LC14.java
@@ -5,13 +5,18 @@ package leetcode;
 import java.util.stream.Stream;
 
 class LC14 {
-    public String solution1(String[] strs) {
+    public String solution1(final String[] strs) {
         if (strs.length == 0) return "";
         var i = 0;
-        for (final var ii = i;
-             Stream.of(strs).allMatch(s -> s.length() > ii
-                                      && s.charAt(ii) == strs[0].charAt(ii));) {
-            i++;
+        while (true) {
+            final var ii = i;
+            final var isSame = Stream.of(strs)
+                .allMatch(s -> s.length() > ii && s.charAt(ii) == strs[0].charAt(ii));
+            if (isSame) {
+                i += 1;
+            } else {
+                break;
+            }
         }
 
         return strs[0].substring(0, i);
